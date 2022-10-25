6bc37de5d89db4a9dfb4360f6e2c4e51e381a840
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-10-24 22:49:30 -0400

    employee test passed

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -5,4 +5,40 @@ const Employee = require("../lib/Employee");
 test("should return instance of new employee object", () => {
         const newHire = new Employee();
         expect(typeof newHire).toBe("object");
-    })
+    });
+
+test("can set name thru constructor", () => {
+    const name = "Bella";
+    const newHire = new Employee(name);
+    expect(newHire.name).toBe(name);
+});
+
+test("can set id thru constructor", () => {
+    const testValue = 100;
+    const newHire = new Employee("newid", testValue);
+    expect(newHire.id).toBe(testValue);
+});
+
+test("can set email thru constructor", () => {
+    const testValue = "example@example.com"
+    const newHire = new Employee("newid", 1, testValue);
+    expect(newHire.email).toBe(testValue);
+});
+
+test("Retrieve name via getName()", () => {
+    const testValue = "Bella";
+    const newHire = new Employee(testValue);
+    expect(newHire.getName()).toBe(testValue);
+  });
+
+  test("Retrieve id via getId()", () => {
+    const testValue = 100;
+    const newHire = new Employee("newid", testValue);
+    expect(newHire.getId()).toBe(testValue);
+  });
+
+  test("Retrieve email via getEmail()", () => {
+    const testValue = "example@example.com";
+    const newHire = new Employee("newid", 1, testValue);
+    expect(newHire.getEmail()).toBe(testValue);
+  });
--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,5 +1,12 @@
 //Team Profile Generator
+const Manager = require("../lib/Manager");
+const Employee = require("../lib/Employee");
 //Initilization
 //Arrange
 //Act
 //Assert
+
+//Team Profile Generator
+
+
+
