e0a33e531bd40bfd564f1fd03e7f111623e28727
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-10-25 20:38:56 -0400

    all test passed

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,5 +1,5 @@
 //Team Profile Generator
-const Manager = require("../lib/Engineer");
+const Engineer = require("../lib/Engineer");
 
 test("can set github thru constructor", () => {
     const testValue = "GitHubUser"
@@ -9,14 +9,14 @@ test("can set github thru constructor", () => {
 
 test("can get get github username via getGithub()", () => {
     const testValue = "GitHub User"
-    const engineer = new Engieer("Bella", 1, "test@example.com", GitHubUser);
+    const engineer = new Engineer("Bella", 1, "test@example.com", testValue);
     expect(engineer.getGithub()).toBe(testValue);
 });
 
 test("getRole() should set role as \"employee\"", () => {
-    const testValue = "Manager"
-    const manager = new Manager("Bella", 1, "bella@example.com", 100);
-    expect(manager.getRole()).toBe(testValue);
+    const testValue = "Engineer"
+    const engineer = new Engineer("Bella", 1, "bella@example.com", 100);
+    expect(engineer.getRole()).toBe(testValue);
 });
 
 
--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,5 +1,23 @@
 //Team Profile Generator
-//Initilization
-//Arrange
-//Act
-//Assert
+const Intern = require("../lib/Intern");
+
+test("Can set school utilizing constructor", () => {
+    const testValue = "TCU"
+    const intern = new Intern("Bella", 1, "test@example.com", testValue);
+    expect(intern.school).toBe(testValue);
+});
+
+test("getRole() should return \"Intern\"", () => {
+    const testValue = "Intern"
+    const intern = new Intern("Bella", 1, "test@example.com", "TCU");
+    expect(intern.getRole()).toBe(testValue);
+});
+
+test("Get school via getSchool()", () => {
+    const testValue = "TCU"
+    const intern = new Intern("Bella", 1, "bella@example.com", testValue);
+    expect(intern.getSchool()).toBe(testValue);
+});
+
+
+
